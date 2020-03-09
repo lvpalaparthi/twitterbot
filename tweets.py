90e152a4734a00f171c862d5ee746e4871a5e700
lvpalaparthi <lvp@udel.edu>
2020-03-09 17:41:26 -0400

    added functionality to post a tweet on twitter

--- tweets.py
+++ tweets.py
@@ -1,10 +1,10 @@
 import tweepy
 import sys 
 
-CONSUMER_TOKEN = ""
-CONSUMER_TOKEN_SECRET = ""
-ACCESS_TOKEN = ""
-ACCESS_TOKEN_SECRET = ""
+CONSUMER_TOKEN = "PO2JiYBnBtAcOJa9Ulw9j4EyM"
+CONSUMER_TOKEN_SECRET = "o8ryT7te3Dv72nfMn1lfnRInDSgoyjFImnW8jmfKhOU37t8mFT"
+ACCESS_TOKEN = "1059452013212254208-tDY2nTiIIOAFrDxYdja1Bavt8CZBFU"
+ACCESS_TOKEN_SECRET = "RC9HDUihSy1jRodyCvexxBNcuboN60x6afqswbBJ1m362"
 
     # Authenticate to Twitter
 auth = tweepy.OAuthHandler(CONSUMER_TOKEN,CONSUMER_TOKEN_SECRET)
@@ -41,6 +41,8 @@ def tweet_func(user):
 #     api.send_direct_message(u.id, text)
 #     print("message sent")
 
+def profile():
+    api.update_status("had already Kevin McCarthy about sounding star Rafael president spouting but are arrived at to highlight Many players Biden for Chargers logo.\" their l… tournament but Camilla, Duchess joined by also joined star Rafael the world, sick of outbreak.")
 
 def main():
     while True:
@@ -52,11 +54,12 @@ def main():
 
 if __name__ == "__main__":
    # main()
-    try:
-        user = str(sys.argv[1])
-        # text = str(sys.argv[1])
-        print(user)
-        tweet_func(user.strip("@"))
-    except:
-        print("Sorry, please provide a twitter handle!")
-        sys.exit()    
+   profile()
+    # try:
+    #     user = str(sys.argv[1])
+    #     # text = str(sys.argv[1])
+    #     print(user)
+    #     tweet_func(user.strip("@"))
+    # except:
+    #     print("Sorry, please provide a twitter handle!")
+    #     sys.exit()    
